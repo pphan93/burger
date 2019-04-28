17777a51667f738007e84e5a1aca130fed43a79c
Phuoc phan <phuocphan12@gmail.com>
2019-04-28 16:30:28 -0400

    done

--- db/schema.sql
+++ db/schema.sql
@@ -10,12 +10,3 @@ CREATE TABLE burgers (
   devoured boolean default true,
   PRIMARY KEY (id)
 );
-
-INSERT INTO products (product_name, department_name, price,quantity)
-VALUES ('Samsung UN65NU7100FXZC 65" 4K Ultra HD Smart LED TV', 'Electronic', 1097.99,10);
-
-INSERT INTO products (product_name, department_name, price,quantity)
-VALUES ('Playstation 4 - 1TB Slim', 'Video Games',  379.99,5);
-
-INSERT INTO products (product_name, department_name, price,quantity)
-VALUES ('ASUS GL504GV-DS74 ROG Strix Gaming Laptop, 15.6” FHD', 'Electronic', 2199.00,9);
